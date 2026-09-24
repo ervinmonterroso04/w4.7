dbfdd691678fe7c081679d1761e541de5fc6fb6d
ervinmonterroso04@gmail.com <ervinmonterroso04@gmail.com>
2026-09-23 21:38:56 -0400

    src

--- src/api/activities.js
+++ src/api/activities.js
@@ -6,8 +6,8 @@ export async function getActivities() {
     const response = await fetch(API + "/activities");
     const result = await response.json();
     return result;
-  } catch (e) {
-    console.error(e);
+  } catch (err) {
+    console.error(err);
     return [];
   }
 }
@@ -28,6 +28,22 @@ export async function createActivity(token, activity) {
       Authorization: "Bearer " + token,
     },
     body: JSON.stringify(activity),
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
+
+export async function deleteActivity(token, id) {
+  if (!token) {
+    throw Error("You must be signed in to delete an activity.");
+  }
+
+  const response = await fetch(API + "/activities" + id, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
   });
 
   if (!response.ok) {
